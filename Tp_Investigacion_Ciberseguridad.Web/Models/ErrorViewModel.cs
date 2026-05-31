8e980ffd1d6041a6b3d257e238e76507a842034a
Laraaa194 <lara.rs1412@gmail.com>
2026-05-31 14:20:23 -0300

    appsettings.Development.json en gitignore + base mvc

---
+++ Tp_Investigacion_Ciberseguridad.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tp_Investigacion_Ciberseguridad.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
